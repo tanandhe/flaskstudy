440c00d9892b701ef88ad19891d0831ca2f13e42
Tanxinyuan <18604892102@qq.com>
2018-10-28 23:29:51 +0800

    Flask Web开发第一章实例

--- flaskstudy/main.py
+++ flaskstudy/main.py
@@ -6,13 +6,27 @@ Created on Sun Oct 28 21:51:24 2018
 """
 
 from flask import Flask
-import time
+from flask import request
+from flask import make_response
+from flask import redirect
+from flask import abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
-
+    user_agent = request.headers.get('User_Agent')
+    res = make_response("<h1>The response carries a cookie</h1>")
+    res.set_cookie('answer','42')
+    return res
+@app.route('/user/<id>')
+def user(id):
+    cookie = request.cookies.get('answer')
+    if not id:
+        abort(404)
+    return cookie+id
+@app.route('/404/')
+def reDirect():
+    return redirect('http://www.baidu.com')
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True,host='0.0.0.0',port=80)
